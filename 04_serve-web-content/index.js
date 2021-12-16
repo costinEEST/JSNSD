8a2bb4587f005f40e67895cb74ac883e47437ad1
Costi <mail.la.costi@gmail.com>
2021-12-16 17:41:28 +0200

    serve HTML content from a static directory

--- 04_serve-web-content/index.js
+++ 04_serve-web-content/index.js
@@ -1,23 +1,27 @@
 import http from "http";
-import { join } from 'path'
+import { join } from "path";
 
 import express from "express";
 import dotenv from "dotenv";
 
-import { __dirname } from '../utils.js'
+import { __dirname } from "../utils.js";
 
 dotenv.config();
 
-
-const server = express();
+const publicDir = join(__dirname, "04_serve-web-content/public");
+const server = express().use(express.static(publicDir));
 const port = process.env.PORT || 3002;
 
-server.set('views', join(__dirname, 'views'))
-server.set('view engine', 'hbs')
+server.set("views", join(__dirname, "views"));
+server.set("view engine", "hbs");
 
-server.get('/me', (_, res) => {
-  res.send('UI dev')
-})
+server.get("/", (_, res) => {
+  res.sendFile(join(publicDir, "index.html"));
+});
+
+server.get("/me", (_, res) => {
+  res.send("UI dev");
+});
 
 http.createServer(server).listen(port, function () {
   console.log("Listening on ", this.address());
